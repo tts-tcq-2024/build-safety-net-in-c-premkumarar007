58ec2511564364ee698ec173125ceb22cc02337d
PREM KUMAR R <80571286+premkumarar007@users.noreply.github.com>
2024-07-02 19:43:59 +0530

    adding more testcases for testing the soundex algorithm

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -5,5 +5,8 @@ TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
  //AAA
   char soundex[5];
   generateSoundex("AX", soundex);
+  generateSoundex("Robert", soundex);
+  generateSoundex("Rupert", soundex);
+  generateSoundex("Pfister", soundex);
  // ASSERT_EQ(soundex,"A200");
 }
