d036afe0575a27ab5661c393758f90546f4ba860
PREM KUMAR R <80571286+premkumarar007@users.noreply.github.com>
2024-07-03 16:37:42 +0530

    refactoring functions for improving CCN further

--- Soundex.h
+++ Soundex.h
@@ -5,18 +5,32 @@
 #include <ctype.h>
 #include <string.h>
 
+// Array to map characters to Soundex codes
 const int soundexMapping[26] = {
-    0, 1, 2, 3, 0, 1, 2, 0, 0, 2,
-    2, 4, 5, 5, 0, 1, 2, 6, 2, 3,
-    0, 1, 0, 2, 0, 2             
+    0, 1, 2, 3, 0, 1, 2, 0, 0, 2, // A-J
+    2, 4, 5, 5, 0, 1, 2, 6, 2, 3, // K-T
+    0, 1, 0, 2, 0, 2             // U-Z
 };
 
 char getSoundexCode(char c) {
     c = toupper(c);
     if (c < 'A' || c > 'Z') {
-        return 0;
+        return '0';  // Non-alphabet characters
     }
-    return soundexMapping[c - 'A'] + '0';
+    return soundexMapping[c - 'A'] + '0';  // Convert digit to char
+}
+
+void appendSoundexCode(char code, char *soundex, int *sIndex) {
+    if (code != '0' && code != soundex[*sIndex - 1]) {
+        soundex[(*sIndex)++] = code;
+    }
+}
+
+void finalizeSoundex(char *soundex, int sIndex) {
+    while (sIndex < 4) {
+        soundex[sIndex++] = '0';
+    }
+    soundex[4] = '\0';
 }
 
 void generateSoundex(const char *name, char *soundex) {
@@ -26,16 +40,11 @@ void generateSoundex(const char *name, char *soundex) {
 
     for (int i = 1; i < len && sIndex < 4; i++) {
         char code = getSoundexCode(name[i]);
-        if (code != '0' && code != soundex[sIndex - 1]) {
-            soundex[sIndex++] = code;
-        }
+        appendSoundexCode(code, soundex, &sIndex);
     }
 
-    while (sIndex < 4) {
-        soundex[sIndex++] = '0';
-    }
-
-    soundex[4] = '\0';
+    finalizeSoundex(soundex, sIndex);
 }
+
 
 #endif // SOUNDEX_H
