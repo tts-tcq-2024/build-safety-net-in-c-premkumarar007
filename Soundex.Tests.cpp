ab08cf9c0c0ec9e624d233343caf423526299423
PREM KUMAR R <80571286+premkumarar007@users.noreply.github.com>
2024-07-05 14:31:22 +0530

    Adding more testcases of Soundex algorithm

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -6,4 +6,8 @@ TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
   char soundex[5];
   generateSoundex("AX", soundex);
   ASSERT_STREQ(soundex,"A200");
+  generateSoundex("Pfister",soundex);
+  ASSERT_STREQ(soundex,"P236");
+  generateSoundex("Ashcraft",soundex);
+  ASSERT_STREQ(soundex,"A261");
 }
