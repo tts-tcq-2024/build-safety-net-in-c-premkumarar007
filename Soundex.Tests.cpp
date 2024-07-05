774762fbff3192b1bdebe9be8350e3fa577d8a2c
PREM KUMAR R <80571286+premkumarar007@users.noreply.github.com>
2024-07-05 14:17:13 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -5,9 +5,5 @@ TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
  //AAA
   char soundex[5];
   generateSoundex("AX", soundex);
-  generateSoundex("Robert", soundex);
-  generateSoundex("Rupert", soundex);
-  generateSoundex("Pfister", soundex);
-  generateSoundex("Prem890", soundex);
   ASSERT_EQ(soundex,"A200");
 }
